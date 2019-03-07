d1c84150d4b33c40be643490ae86ee3acf9145da
Margaret O'Neill <margaret@oneillfish.com>
2019-03-07 14:05:08 -0500

    oops, forgot.  html updating.  some css styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,34 @@
 
 //create an element that will be inserted into HTML
 const wDiv = document.createElement('div');
-wDiv.textContent = "Weather Data";
+// wDiv.textContent = "Weather Data";
+wDiv.className = "wDiv";
 
 //get a pointer for my existing DIV on the page where this will be inserted
-currentWeaterDiv = document.querySelector('[data-weather]');
+currentWeatherDiv = document.querySelector('[data-weather]');
 
+//create individual elements to hold the data:
+function appendElement(tagname,value){
+    const wSubDiv = document.createElement('div');
+    wSubDiv.textContent =  tagname + ":" + value;
+    // console.log(wSubDiv);
+    wDiv.append(wSubDiv);
+}
 //use the weater data to create html elements
+appendElement('City Name',getLocationName(atlWeather));
+appendElement('Current Temperature',getTemperature(atlWeather));
+appendElement('Wind Speed',getWindSpeed(atlWeather));
+
+//Then append that conglomerate div element to the existing page
+currentWeatherDiv.appendChild(wDiv);
 
 // Implement the following functions to fulfill the tests!
 function getLocationName(data) {
     return data.name;
+}
+function getTemperature(data) {
+    //and convert from Kelvin
+    return parseInt(((data.main.temp -273) * 9 / 5) + 32);
 }
 function getLocationCountry(data) {
     return data.sys.country;
@@ -32,20 +50,3 @@ function getSunrise(data) {
     return data.sys.sunrise;
 }
 
-// console.log(`The location is ${getLocationName(atlWeather)}`);
-// console.log(getLocationCountry(atlWeather));
-// console.log(atlWeather.weather.description);
-// Please ignore the following
-try {
-    module.exports = {
-        getLocationName,
-        getLocationCountry,
-        getLocationLatitude,
-        getLocationLongitude,
-        getDescription,
-        getWindSpeed,
-        getSunrise
-    }
-} catch (e) {
-    
-}
